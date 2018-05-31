075763fcf45138d4fec714af94c0dca3d96d6cfc
Bryan Kyamaggwa <bryankyamaggwa@gmail.com>
2018-05-31 17:37:13 +0300

    hello to hallo

--- index.php
+++ index.php
@@ -108,7 +108,7 @@ _________________________________________________________ -->
                                 </button>
                             </div>
                         </div>
-                        <div class="navbar-collapse collapse" id="navigation hello">
+                        <div class="navbar-collapse collapse" id="navigation hallo">
 
                             <ul class="nav navbar-nav navbar-right">
                                 <li><a href="#" title="Athena Hotel Homepage">Home</a></li>
