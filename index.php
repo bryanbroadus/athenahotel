6773e92f61197d772146ee751f67096afdf68878
Bryan Kyamaggwa <bryankyamaggwa@gmail.com>
2018-05-31 17:28:29 +0300

    nope2

--- index.php
+++ index.php
@@ -108,7 +108,7 @@ _________________________________________________________ -->
                                 </button>
                             </div>
                         </div>
-                        <div class="navbar-collapse collapse" id="navigation nope">
+                        <div class="navbar-collapse collapse" id="navigation nope2">
 
                             <ul class="nav navbar-nav navbar-right">
                                 <li><a href="#" title="Athena Hotel Homepage">Home</a></li>
